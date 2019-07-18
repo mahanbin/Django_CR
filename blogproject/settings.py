794b94bcee3e6fd27654be665ce2d5f2c69c1632
hanbin <hanbin@likelion.net>
2019-07-18 13:29:09 +0900

    upload project

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'portfolio.apps.PortfolioConfig',
-    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
--- blogproject/urls.py
+++ blogproject/urls.py
@@ -19,12 +19,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 import blog.views
 import portfolio.views
-import accounts.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',blog.views.home, name='home'),
     path('blog/', include('blog.urls')),
     path('portfolio/',include('portfolio.urls')),
-    path('accounts/',include('accounts.urls')),
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
